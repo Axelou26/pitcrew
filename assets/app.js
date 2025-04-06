0a56f7b0c3e039ab9eb31bd13e0264b7fcce3ee2
Axelou26 <axel.mancini@ynov.com>
2025-04-06 22:22:12 +0200

    Mise à jour de la configuration Vite et des imports Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,11 @@
+// Import styles
 import './styles/app.scss';
 
+// Import Bootstrap JS
+import * as bootstrap from 'bootstrap';
+
+// Make Bootstrap available globally
+window.bootstrap = bootstrap;
+
 // Import Bootstrap
-import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css'; 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import symfonyPlugin from 'vite-plugin-symfony';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +11,19 @@ export default defineConfig({
             input: {
                 app: './assets/app.js'
             },
+        }
+    },
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '@': path.resolve(__dirname, './assets')
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "bootstrap/scss/bootstrap";`
+            }
         }
     }
 }); 
