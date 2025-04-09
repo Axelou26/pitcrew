11b683a64795152f2ca759e7e0d77101e5c1918d
Axelou26 <axel.mancini@ynov.com>
2025-04-09 14:01:35 +0200

    fix: correction des interfaces de formulaire pour la compatibilité avec Symfony

--- src/Form/Type/InterviewTypeInterface.php
+++ src/Form/Type/InterviewTypeInterface.php
@@ -3,12 +3,12 @@
 namespace App\Form\Type;
 
 use App\Entity\Interview;
+use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-interface InterviewTypeInterface
+interface InterviewTypeInterface extends FormTypeInterface
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void;
     public function configureOptions(OptionsResolver $resolver): void;
-    public function getBlockPrefix(): string;
 } 
--- src/Form/Type/JobOfferTypeInterface.php
+++ src/Form/Type/JobOfferTypeInterface.php
@@ -2,12 +2,12 @@
 
 namespace App\Form\Type;
 
+use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-interface JobOfferTypeInterface
+interface JobOfferTypeInterface extends FormTypeInterface
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void;
     public function configureOptions(OptionsResolver $resolver): void;
-    public function getBlockPrefix(): string;
 } 
